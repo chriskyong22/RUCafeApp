904fd0e47b198dddc811dbdffb07b916982ca0c2
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2021-04-07 23:56:12 -0400

    Added ability to switch between activities and updated gitignore

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_storedorders);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchCoffeeActivity(View v) {
+        Intent intent = new Intent(this, CoffeeOrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchDonutActivity(View v) {
+        Intent intent = new Intent(this, DonutOrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchCurrentOrderActivity(View v) {
+        Intent intent = new Intent(this, CurrentOrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchStoredOrdersActivity(View v) {
+        Intent intent = new Intent(this, StoredOrdersActivity.class);
+        startActivity(intent);
     }
 }
