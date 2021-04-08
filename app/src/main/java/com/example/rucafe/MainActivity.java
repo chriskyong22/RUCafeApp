0994e888d18fe22737216797e348276e5dfb5a50
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2021-04-08 00:23:36 -0400

    Added parent activity in AndroidManifest for the other activites besides main and changed primary color to red

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.menu_title);
     }
 
     public void launchCoffeeActivity(View v) {
